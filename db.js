7a322ab7ecb996a2971211cdc96d167224d0f8a7
IvanGonzalez10 <ivan.c.gonzalez1@gmail.com>
2020-12-07 23:23:35 -0500

    Making json request

---
+++ db.js
@@ -0,0 +1,28 @@
+var data = [
+  {
+    "id": "1",
+    "url": "https://images.unsplash.com/photo-1593642533144-3d62aa4783ec?ixid=MXwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    "id": "2",
+    "url": "https://images.unsplash.com/photo-1607212053115-d85fa8fddde4?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    "id": "3",
+    "url": "https://images.unsplash.com/photo-1607344965879-a0fb7485f10a?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    "id": "4",
+    "url": "https://images.unsplash.com/photo-1607350541199-f17ea6717823?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+  },  {
+    "id": "5",
+    "url": "https://images.unsplash.com/photo-1607326005199-f088b22a9d69?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    "id": "6",
+    "url": "https://images.unsplash.com/photo-1607336946504-76af179a7413?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+  },  {
+    "id": "7",
+    "url": "https://images.unsplash.com/photo-1607340386024-e5ccf0cb48dd?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+  }
+];
---
+++ script.js
@@ -0,0 +1,5 @@
+var contenedor = document.getElementById('data');
+data.map((el) => {
+  contenedor.appendChild()
+})
+console.log(contenedor)
