e4f5e30b145276d79bc725133bba9319fe3109a3
IvanGonzalez10 <ivan.c.gonzalez1@gmail.com>
2020-12-08 00:17:04 -0500

    fix json

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+function traer() {
+  fetch("https://raw.githubusercontent.com/IvanGonzalez10/TitaTest/master/db.json")
+  .then (data => {
+    console.log(data.json())
+  })
+}
+
+traer()
+
 var contenedor = document.getElementById("data");
-data.map((el, i) => {
+/* data.map((el, i) => {
   var item = document.createElement("div");
   item.className=`item item${i}`;
   var imagen = document.createElement("img");
   imagen.src=el.url;
   item.appendChild(imagen)
   contenedor.appendChild(item);
-});
-console.log(contenedor);
+}); */
