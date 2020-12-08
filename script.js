c8ed65febc799b1c016864e64509a1a6b26bdb4b
IvanGonzalez10 <ivan.c.gonzalez1@gmail.com>
2020-12-08 12:11:28 -0500

    cellphone media query

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const menu = document.querySelector('.nav-container');
+const burgerButton = document.querySelector('#burger-button');
+
+burgerButton.addEventListener('click', hideMenu)
+
+function hideMenu(){
+  if (menu.classList.contains('is-active')){
+    menu.classList.remove('is-active');
+  } else {
+    menu.classList.add('is-active');
+  }
+}
+
 function traer() {
   fetch(
     "https://raw.githubusercontent.com/IvanGonzalez10/TitaTest/master/db.json"
