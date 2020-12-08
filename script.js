51749d19a8396da26eeacd987510e5234f6d00c4
IvanGonzalez10 <ivan.c.gonzalez1@gmail.com>
2020-12-07 23:44:23 -0500

    json to repository

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-var contenedor = document.getElementById('data');
-data.map((el) => {
-  contenedor.appendChild()
-})
-console.log(contenedor)
+var contenedor = document.getElementById("data");
+data.map((el, i) => {
+  var item = document.createElement("div");
+  item.className=`item item${i}`;
+  var imagen = document.createElement("img");
+  imagen.src=el.url;
+  item.appendChild(imagen)
+  contenedor.appendChild(item);
+});
+console.log(contenedor);
