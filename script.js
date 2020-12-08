aae60d7a51cadb7d539041945764340c9de9e5cb
IvanGonzalez10 <ivan.c.gonzalez1@gmail.com>
2020-12-08 00:21:20 -0500

    json complited

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 function traer() {
-  fetch("https://raw.githubusercontent.com/IvanGonzalez10/TitaTest/master/db.json")
-  .then (data => {
-    console.log(data.json())
-  })
+  fetch(
+    "https://raw.githubusercontent.com/IvanGonzalez10/TitaTest/master/db.json"
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      var contenedor = document.getElementById("data");
+      data.map((el, i) => {
+        var item = document.createElement("div");
+        item.className = `item item${i}`;
+        var imagen = document.createElement("img");
+        imagen.src = el.url;
+        item.appendChild(imagen);
+        contenedor.appendChild(item);
+      });
+    });
 }
 
-traer()
-
-var contenedor = document.getElementById("data");
-/* data.map((el, i) => {
-  var item = document.createElement("div");
-  item.className=`item item${i}`;
-  var imagen = document.createElement("img");
-  imagen.src=el.url;
-  item.appendChild(imagen)
-  contenedor.appendChild(item);
-}); */
+traer();
